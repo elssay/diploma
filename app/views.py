fa1ad1622a726870b6560b5586cc318c440c1cbf
elssay <sveta-zvezda-poeta@yandex.ru>
2020-11-10 14:11:45 +0300

    Rel5

--- app/views.py
+++ app/views.py
@@ -71,12 +71,13 @@ def create_auto():
 @app.route('/auto_detail/<int:auto_id>', methods=['POST', 'GET'])
 def auto_detail(auto_id):
     
-    auto = Auto.query.get(auto_id)
+    auto = Auto.query.get(auto_id) 
     context = None
-    free = 'свободен'
-    button_name = 'Арендовать'
+    free = 'свободен' #статус авто (для Frontend) в зависимости от состояния кнопки "Арендовать/Освободить"
+    button_name = 'Арендовать' # название кнопки в зависимости от статуса
+    transmission_type = '' # "да" или "нет" в зависимости от того, автоматическая КПП или нет
     
-    if auto.astatus == False:
+    if auto.astatus == False: #поле таблицы Auto, False - если находится в аренде(занят)
         free = 'занят'
         button_name = 'Освободить'
         
@@ -84,9 +85,14 @@ def auto_detail(auto_id):
     elif auto.astatus == True:
         free = 'свободен'
         button_name = 'Арендовать'
+
+    if auto.transmission == True:
+        transmission_type = 'да'
+    elif auto.transmission == False:
+        transmission_type = 'нет'
         
     if request.method == 'POST':
-
+        #если нужно изменить информацию об авто:
         new_title = request.form['new_title']
         new_price = request.form['new_price']
         new_description = request.form['new_description']
@@ -107,9 +113,9 @@ def auto_detail(auto_id):
             auto.description = request.form['new_description']
 
 
-        if auto_transmission_check == 'option1':
+        if auto_transmission_check == 'option1': #пользователь выбрал "да" в поле "Автоматическая КПП"
             auto.transmission = True
-        elif auto_transmission_check == 'option2':
+        elif auto_transmission_check == 'option2': #пользователь выбрал "нет" в поле "Автоматическая КПП"
             auto.transmission = False
               
         if new_img_url:
@@ -120,10 +126,10 @@ def auto_detail(auto_id):
             auto.img_url_3 = request.form['new_img_url3']
         if new_img_url_4:
             auto.img_url_4 = request.form['new_img_url4']
-        db.session.commit() 
+        db.session.commit() #сохраняем новую введенную пользователем информацию в поля таблицы БД
 
     
-
+    #получаем историю аренды для текущего автомобиля:
     rentlog = Rentlog.query.filter_by(auto_id=auto.id).all()
         
     
@@ -133,6 +139,7 @@ def auto_detail(auto_id):
         'title': auto.title,
         'price': auto.autprice,
         'description': auto.description,
+        'transmission': transmission_type,
         'img_url': auto.img_url,
         'img_url_2': auto.img_url_2,
         'img_url_3': auto.img_url_3,
@@ -141,14 +148,13 @@ def auto_detail(auto_id):
         'button_name': button_name,
         'rentlog': rentlog,
     }
-    #db.session.commit() 
-
+    
 
     return render_template('auto_detail.html', **context)
 
 
 @app.route('/rent_auto/<int:auto_id>', methods=['POST', 'GET'])
-def auto_rent(auto_id):
+def auto_rent(auto_id): #функция арендовать/освободить авто
     
     auto = Auto.query.get(auto_id)
     
@@ -157,24 +163,27 @@ def auto_rent(auto_id):
     
     if request.method == 'POST':
         new_status = not auto.astatus
-        if new_status == False:
-            free = 'занят'
-            auto.aurented = datetime.now()
-            auto.date_end = None
-            total_price = 0
+        if new_status == False: #автомобиль свободен
+            free = 'занят' #при нажатии на кнопку "Арендовать" статус становится "занят"
+            auto.aurented = datetime.now() #записываем время нажатия на кнопку "Арендовать" в поле БД "начало аренды"
+            auto.date_end = None #поле "конец аренды" пока пустое
+            total_price = 0 #стоимость аренды 
             db.session.commit()
 
         elif new_status == True:
-            free = 'свободен'
-            auto.date_end = datetime.now()
-            age_seconds = (auto.date_end - auto.aurented).seconds
-            age = divmod(age_seconds, 60)
-            total_price = age[0] * auto.autprice
-            auto.atotal_time += age[0]
-            auto.total_price += total_price
-            auto.rent_count+=1
+            free = 'свободен' #автомобиль занят
+            auto.date_end = datetime.now() #при нажатии на кнопку "Освободить" в поле "конец аренды" записываем время нажатия на кнопку
+            age_seconds = (auto.date_end - auto.aurented).seconds #получаем время аренды в секундах
+            age = divmod(age_seconds, 60) #получаем кортеж из минут и секунд времени аренды
+            total_price = age[0] * auto.autprice #получаем стоимость аренды, умножив количество минут на стоимость аренды в минуту
+            auto.atotal_time += age[0] #добавляем минуты в поле "общее время аренды" авто
+            auto.total_price += total_price #добавляем стоимость аренды в поле "общая стоимость аренды" авто
+            auto.rent_count+=1 #увеличиваем на 1 общее количесвто бронирований авто
+
+            #добавляем запись об аренде авто в БД:
             db.session.add(Rentlog(auto_id=auto.id, rented = auto.aurented, date_end = auto.date_end, rentprice=total_price))
             db.session.commit()
+
         auto.astatus = new_status
         db.session.commit()
 
@@ -192,7 +201,7 @@ def auto_rent(auto_id):
 
 
 @app.route('/del_auto/<int:auto_id>', methods=['POST'])
-def del_auto(auto_id):
+def del_auto(auto_id): #функция удаления авто
     
     auto = Auto.query.get(auto_id)
     
@@ -200,14 +209,7 @@ def del_auto(auto_id):
     context = {
         'id': auto.id,
         'title': auto.title,
-        'price': auto.autprice,
-        'description': auto.description,
-        'astatus': auto.status,
-        'status': auto.astatus,
-        'transmission': auto.transmission,
-        'id': auto.id,
-        'arented': auto.aurented,
-        #'aend_of_rent': auto.data_end,
+        
         }
     
     db.session.delete(auto)
@@ -217,19 +219,12 @@ def del_auto(auto_id):
 
 
 @app.route('/rental_log/', methods=['GET'])
-def rental_log():
+def rental_log(): #функция вывода журнала аренды для каждого авто:
     
     auto_list = Auto.query.all()
-    #auto = Auto.query.get(auto_id)
-
-  
-    # Полученные наборы передаем в контекст
+    
     context = {
-        # 'title': auto.title,
-        'auto_list': auto_list,
-        # 'count':auto.rent_count,
-        # 'total_time': auto.total_time,
-        # 'total_price': auto.total_price,
+        'auto_list': auto_list, 
     }
 
     return render_template('rental_log.html', **context)
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import os
 from app import app
 
 
-app.run(debug = True)
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    app.run(debug = True)
